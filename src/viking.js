fef5026483deb2a6360bcef7726e17f1f4eea381
nikolasMendes <nikolasmendes98@hotmail.com>
2023-01-16 20:18:29 -0300

    done 1

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,74 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = 100;
+        this.strength = 30;
 
-// Viking
-class Viking {}
+    }
+    attack() {
+        return this.strength
 
-// Saxon
-class Saxon {}
+    }
+    receiveDamage(damage) {
+        this.health = this.health - damage
+        return this.health
+    }
+  } 
+  
+  // Viking
+  class Viking extends Soldier {
+    constructor (health, strength, name)
+    super (health,strength) {
 
-// War
-class War {}
+        this.name = Viking
+    }
+    receiveDamage(damage) {
+        this.health = this.health - damage
+        return this.health
+        if (this.health > 0) {
+            return "Viking has received DAMAGE points of damage"}
+        if (this.health = 0) {
+            return "Viking has died in act of combat"}
+        }
+        
+    battleCry() {
+            return "Odin Owns You All!"
+        }
+
+    }
+  
+  
+  // Saxon
+  class Saxon extends Soldier {
+    attack() {
+        return this.strength
+    }
+    receiveDamage(damage){
+        this.health = this.health - damage
+        return this.health
+        if (this.health > 0) {
+            return "A Saxon has received DAMAGE points of damage"}
+        if (this.health = 0) {
+            return "A Saxon has died in act of combat"}
+    }
+  }
+
+  
+  // War
+  class War {
+    addViking() {
+
+    }
+addSaxon() {
+
+}
+vikingAttack() {
+
+}
+saxonAttack() {
+
+}
+showStatus() {
+
+}
+  }
